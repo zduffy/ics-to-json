466cb33f12ca4a64bc6de1fa5a2ffd148c6c60ae
zduffy <zach.duffy@icloud.com>
2023-02-16 23:45:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-var icsToJson = require("ics-to-json")
-// Get ICS text however you like, example below
-// Make sure you have the right CORS settings if needed
-var fileLocation = 'https://www.exclusiveautowraps.com/?calendarista_handler=feeds&calendarista_appkey=CA60a1e9db559a8&calendarista_service=9&calendarista_status=-1';
-const convert = async (fileLocation) => {
-    const icsRes = await fetch(fileLocation)
-    const icsData = await icsRes.text()
-    // Convert
-    const data = icsToJson(icsData)
-    return data
-}
+var luxon = require("luxon")
+var ical = require("node-ical")
+ical.async.fromURL('https://www.exclusiveautowraps.com/?calendarista_handler=feeds&calendarista_appkey=CA60a1e9db559a8&calendarista_service=9&calendarista_status=-1', function(err, data) { console.log(data); });
